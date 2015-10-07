7c131a444bdf0bed409cc32397192a49e48c9f1e
Kevin Chen <kevin@kevinchen.co>
2015-10-07 19:53:30 -0400

    Add C++ example of Twinkle.
    There are probably some platform-specific paths in the code.

---
+++ examples_cpp/install_dependencies.sh
@@ -0,0 +1,16 @@
+platform='unknown'
+uname_str=`uname`
+if [[ "$uname_str" == 'Linux' ]]; then
+   platform='linux'
+elif [[ "$uname_str" == 'Darwin' ]]; then
+   platform='osx'
+fi
+
+echo -ne "Press return to install stk: "
+read
+
+if [[ $platform == 'linux' ]]; then
+   sudo apt-get install stk
+elif [[ $platform == 'osx' ]]; then
+   brew install stk
+fi
