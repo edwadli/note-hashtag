96be91b26af1faab0ca9abac41f3010056f4fd1a
Edward Li <el2724@columbia.edu>
2015-10-14 18:42:10 -0400

    updated dependency script

--- examples_cpp/install_dependencies.sh
+++ examples_cpp/install_dependencies.sh
@@ -10,7 +10,7 @@ echo -ne "Press return to install stk: "
 read
 
 if [[ $platform == 'linux' ]]; then
-   sudo apt-get install stk
+   sudo apt-get install stk stk-doc libstk0-dev
 elif [[ $platform == 'osx' ]]; then
    brew install stk
 fi
