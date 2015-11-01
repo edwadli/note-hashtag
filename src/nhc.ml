0f1c04bcc41e557af7ff01b19225df0cbb2e592e
Kevin Chen <kevin@kevinchen.co>
2015-11-01 18:41:55 -0500

    [Fix #45] log: Create logging module.

--- src/nhc.ml
+++ src/nhc.ml
@@ -1,7 +1,8 @@
 open Ast
+open Log
 open Test
 
 let _ =
   let lexbuf = Lexing.from_string "123\n" in
   let _ = Parser.program Scanner.token lexbuf in
-  print_endline "🎵 #️⃣ c: scanner and parser ran without crashing"
+  Log.info "scanner and parser ran without crashing";
