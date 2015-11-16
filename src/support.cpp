616792d0623e948b1c356d3b66ce1a4c2641f010
Kevin Chen <kevin@kevinchen.co>
2015-11-16 11:26:49 -0500

    lib: Add int/float/string conversion functions.

--- src/support.cpp
+++ src/support.cpp
@@ -1,7 +1,13 @@
-
 #include "support.hpp"
 
-void print_string(std::string output){
+void nh_support::print_string(std::string output) {
   std::cout << output;
 }
 
+int64_t nh_support::int_of_float(double n) {
+  return (int64_t)n;
+}
+
+double nh_support::float_of_int(int64_t n) {
+  return (double)n;
+}
--- src/support.hpp
+++ src/support.hpp
@@ -1,6 +1,11 @@
-
-
+#include <cmath>
 #include <iostream>
 #include <string>
 
-void print_string(std::string);
+namespace nh_support
+{
+  void print_string(std::string);
+  
+  int64_t int_of_float(double n);
+  double float_of_int(int64_t n);
+}
