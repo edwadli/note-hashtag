0f9ceda9cbdfcfccb77d10bdd76a491ac5e420d0
Kevin Chen <kevin@kevinchen.co>
2015-11-16 12:34:06 -0500

    nhc: Turn on clang -Weverything.
    This will help us find subtle problems in our generated C++.

--- src/nhc.ml
+++ src/nhc.ml
@@ -21,7 +21,7 @@ let do_compile src_path bin_path keep_ast keep_il =
     let il_path = bin_path ^ ".cpp" in
     Out_channel.write_all il_path ~data:cpp
   else ();
-  let cpp_compile = "clang++ -Wall -pedantic -O2 -xc++ - support.cpp" in
+  let cpp_compile = "clang++ -Weverything -O2 -xc++ - support.cpp" in
   let ch = Unix.open_process_out cpp_compile in
   Out_channel.output_string ch cpp;
   ignore(Unix.close_process_out ch)
--- src/support.cpp
+++ src/support.cpp
@@ -5,9 +5,9 @@ void nh_support::print_string(std::string output) {
 }
 
 int64_t nh_support::int_of_float(double n) {
-  return (int64_t)n;
+  return static_cast<int64_t>(n);
 }
 
 double nh_support::float_of_int(int64_t n) {
-  return (double)n;
+  return static_cast<double>(n);
 }
