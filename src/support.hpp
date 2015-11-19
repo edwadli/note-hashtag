2e87f2fa9ebdea1e8dd72c4993cd28ce36fc9b68
Kevin Chen <kevin@kevinchen.co>
2015-11-19 01:08:36 -0500

    support: Add concatenation helper.

--- src/support.hpp
+++ src/support.hpp
@@ -1,6 +1,7 @@
 #include <cmath>
 #include <iostream>
 #include <string>
+#include <vector>
 
 typedef int64_t unit;
 const unit LIT_UNIT = 0;
@@ -11,4 +12,12 @@ namespace nh_support
   
   int64_t int_of_float(double n);
   double float_of_int(int64_t n);
+  
+  template<typename T>
+  std::vector<T> concat(std::vector<T> v1, std::vector<T> v2)
+  {
+    std::vector<T> result(v1);
+    std::copy(v2.begin(), v2.end(), std::back_inserter(result));
+    return result;
+  }
 }
