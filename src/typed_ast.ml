edwadli/note-hashtag

commit 7c7472f78748ce8fa5569b9711f387a8825e921b

fixed block environment update

src/typed_ast.ml
@@ -229,8 +229,8 @@ let rec sast_expr env tfuns_ref = function
       else failwith ("There is no function named " ^ name)
   
   | Ast.Block(exprs) ->
-    let texprs = List.rev (List.fold_left exprs ~init:[]
-      ~f:(fun texprs expr ->
+    let (texprs,_) = List.rev (List.fold_left exprs ~init:([],env)
+      ~f:(fun (texprs, env) expr ->
         (* propagate any env changes within block (due to new var initialization) *)
         let env =
           match texprs with
@@ -244,7 +244,7 @@ let rec sast_expr env tfuns_ref = function
                 | _ -> env
               end
         in let texpr = sast_expr env tfuns_ref expr in
-        texpr :: texprs
+        (texpr :: texprs, env)
       ))
     in
     begin match List.last texprs with

src/cast.ml
@@ -58,7 +58,6 @@ type incl =
   | IncludeQuote of string
 
 type signature =
-  | SigStruct of string
   | SigFunc of string * Ast.t * decl list
 
 type program = incl list * signature list * decl list * struct_decl list * func_decl list
@@ -135,7 +134,6 @@ let string_of_sdecl sdecl =
     " {}" ^ "\n};\n"
 
 let string_of_signature = function
-  | SigStruct(name) -> "struct "^name
   | SigFunc(name, tret, decls) ->
       string_of_type tret ^ " " ^ name ^ "(" ^
       String.concat ~sep:", " (List.map decls ~f:(fun (t,_) -> string_of_type t)) ^

src/cpp_sast.ml
@@ -81,8 +81,7 @@ let casttype_of_sasttype (Sast.TDefault(name, fields)) =
     sargs = sargs;
   }
   
-let cast_signatures types fundefs =
-  (List.map types ~f:(fun (Sast.TDefault(n,_)) -> Cast.SigStruct(n))) @
+let cast_signatures fundefs =
   (List.map fundefs ~f:(fun (Sast.FunDef(n,args,(_,tret))) ->
     let decls = List.map args ~f:(fun (s,t)->(t,s)) in
     Cast.SigFunc(n,tret,decls)))
@@ -96,7 +95,7 @@ let cast_of_sast (incls, fundefs, texprs, types) =
   let cast_incls = cast_inclus incls in
   let cast_fundefs = List.map fundefs ~f:(castfun_of_sastfun) in
   let cast_types = List.map types ~f:(casttype_of_sasttype) in
-  let signatures = cast_signatures types fundefs in
+  let signatures = cast_signatures fundefs in
   let globals = [] in
   let main_expr = (Sast.Block(texprs @ [(Sast.LitInt(0),Ast.Int)]),Ast.Int) in
   cast_incls, signatures, globals, cast_types, castfun_of_sastfun (Sast.FunDef("main",[],main_expr))::cast_fundefs

commit 8c6c1b5c8423fd5bacea7d3f0e42050945ab1153

Fix fix block environment update.
The environment updates were being stored backwards.

Bug introduced in 7c7472f78748ce8fa5569b9711f387a8825e921b

src/typed_ast.ml
@@ -229,7 +229,7 @@ let rec sast_expr env tfuns_ref = function
       else failwith ("There is no function named " ^ name)
   
   | Ast.Block(exprs) ->
-    let (texprs,_) = List.rev (List.fold_left exprs ~init:([],env)
+    let (texprs,_) = List.fold_left exprs ~init:([],env)
       ~f:(fun (texprs, env) expr ->
         (* propagate any env changes within block (due to new var initialization) *)
         let env =
@@ -245,8 +245,9 @@ let rec sast_expr env tfuns_ref = function
               end
         in let texpr = sast_expr env tfuns_ref expr in
         (texpr :: texprs, env)
-      ))
+      )
     in
+    let texprs = List.rev texprs in
     begin match List.last texprs with
       | Some(_, t) -> Block(texprs), t
       | None -> LitUnit, Ast.Unit