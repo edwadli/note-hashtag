e371bdc2f8b52bafbf519d9270b36d335c204c39
Kevin Chen <kevin@kevinchen.co>
2015-12-15 10:20:15 -0500

    noincl_ast: Better search, include actually works, refactor.
    Fix #75

--- src/noincl_ast.ml
+++ src/noincl_ast.ml
@@ -1,36 +1,44 @@
-
 open Core.Std
-open Ast
 
-type program = Ast.fundef list * Ast.expr list * Ast.typedef list
+type program = Ast.fundef list * Ast.externfun list * Ast.expr list * Ast.typedef list
 
-let get_inchan = function
-  | None -> In_channel.stdin
-  | Some filename -> In_channel.create filename
+let ast_of_inchan inchan = Parser.program Scanner.token (Lexing.from_channel inchan)
 
-let rec noinclu incls_ref (incls, fdefs, externs, exprs, tdefs) =
-  List.fold_left (List.rev incls) ~init:(fdefs,externs,exprs,tdefs,incls_ref)
-    ~f:(fun (fdefs,externs,exprs,tdefs,incls_ref) next_incl ->
-      let (next_fdefs,next_externs,next_exprs,next_tdefs,incls_ref) = 
+let rec noinclu incls_ref pwd (incls, fdefs, externs, exprs, tdefs) =
+  (* The user just types "include myfile" so we need to prepend the search directory and append the extension *)
+  let incls = List.map incls ~f:(fun incl -> Filename.concat pwd (incl ^ ".nh")) in
+  (* Grab ASTs for all the includes *)
+  let incl_fdefs, incl_externs, incl_exprs, incl_tdefs = List.fold_left incls ~init:([], [], [], [])
+    ~f:(fun (fdefs, externs, exprs, tdefs) next_incl ->
+      let next_fdefs, next_externs, next_exprs, next_tdefs =
         (* for each file, ast recursively *)
-        if List.mem !incls_ref next_incl
-          then
-            (fdefs,externs,exprs,tdefs,incls_ref)
-          else
-            let inchan = get_inchan next_incl in
-            let lexbuf = Lexing.from_channel inchan in
-            let (incls,fdefs,externs,exprs,tdefs) = Parser.program Scanner.token lexbuf in
-            let next_ast = (List.map incls ~f:(fun s -> Some(s)),fdefs,externs,exprs,tdefs) in
-            begin
-              incls_ref := next_incl :: !incls_ref;
-              noinclu incls_ref next_ast
-            end
+        if List.mem !incls_ref next_incl then
+          [], [], [], []
+        else
+          let inchan = In_channel.create next_incl in
+          let next_ast = ast_of_inchan inchan in
+          begin
+            let realpath = Filename.realpath next_incl in
+            incls_ref := realpath :: !incls_ref;
+            noinclu incls_ref (Filename.dirname realpath) next_ast
+          end
       in
-      (next_fdefs@fdefs, next_externs@externs, next_exprs@exprs, next_tdefs@tdefs, incls_ref)
+      (fdefs @ next_fdefs, externs @ next_externs, exprs @ next_exprs, tdefs @ next_tdefs)
     )
+  in
+  (* Prepend the included ASTs to our AST *)
+  incl_fdefs @ fdefs, incl_externs @ externs, incl_exprs @ exprs, incl_tdefs @ tdefs
 
-let noinclu_ast name =
-  (* keep track of already included files *)
-  let incls_ref = ref [] in
-  let (new_fdefs, new_externs, new_exprs, new_tdefs, _) = noinclu incls_ref ([Some("../lib/std.nh"); name],[],[],[],[]) in
-  (new_fdefs, new_externs, new_exprs, new_tdefs)
+let ast_of_filename name =
+  (* toplevel ast *)
+  let get_inchan = function
+    | None -> In_channel.stdin
+    | Some(filename) -> In_channel.create filename
+  in
+  let (incls, fdefs, externs, exprs, tdefs) = ast_of_inchan (get_inchan name) in
+  try
+    (* keep track of already included files *)
+    let seen_incls = ref (match name with None -> [] | Some(x) -> [ Filename.realpath x ]) in
+    let pwd = match name with None -> "./" | Some(x) -> Filename.dirname x in
+    noinclu seen_incls pwd ("../lib/std" :: incls, fdefs, externs, exprs, tdefs)
+  with Unix.Unix_error(error, _, fname) -> failwith (sprintf "%s: %s" fname (Unix.error_message error))
